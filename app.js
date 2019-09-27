8ed549677d3ce333f8289395efa2c873a2b91299
Matias Mirotta <matias.mirotta@bp-4.com>
2019-09-26 21:43:15 -0300

    Cors

--- app.js
+++ app.js
@@ -14,9 +14,19 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitaldb', (err, res) =
     console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
 });
 
+// Habilito cors
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    next();
+});
+
 // Body parser
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
 
 // Importar Rutas
 var appRoutes = require('./routes/app');
